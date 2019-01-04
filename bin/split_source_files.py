1a9658b2c2b413271ee5e4d5a88e1d2affa96612
wl <wanglei1201@sohu.com>
2019-01-04 10:36:27 +0800

    添加对TS文件的前处理脚本

---
+++ bin/split_source_files.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@version: v1.0
+@author: Admin 
+@file: split_source_files.py
+@time: 2019/1/3 0003
+"""
+import os
+
+import datetime
+
+
+class Main():
+    def __init__(self):
+        pass
+
+def filter_files(path):
+    file_list = []
+    for root,dirs,files in os.walk(path):
+        for file_name in files:
+            if 'dat' in file_name:
+                file_list.append(file_name)
+    for end_file_name in file_list:
+        print end_file_name
+    return file_list
+
+def split_files(files_name_list,path):
+    for file_name in files_name_list:
+        new_file_name_YSHJXX = "BDtaishi.YSHJXX." + datetime.datetime.now().strftime('%Y%m%d%H%M%S%f') + ".dat"
+        new_file_name_TYTSMB = "BDtaishi.TYTSMB." + datetime.datetime.now().strftime('%Y%m%d%H%M%S%f') + ".dat"
+        with open(path + "/" + file_name, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                if "YSHJXX" in line:
+                    fo1 = open(path + "/" + new_file_name_YSHJXX, 'a')
+                    str_list1 = line.split(',')[8:]
+                    str1 = ",".join(str_list1)
+                    fo1.write(str1)
+                    fo1.close()
+                elif "TYTSMB" in line:
+                    fo2 = open(path + "/" + new_file_name_TYTSMB, 'a')
+                    str_list2 = line.split(',')[8:]
+                    str2= ",".join(str_list2)
+                    fo2.write(str2)
+                    fo2.close()
+                else:
+                    pass
+
+
+if __name__ == '__main__':
+    path = "/xdata/zcData/sourceFiles/MSGDAT/Ts"
+    files_name_list = filter_files(path)
+    split_files(files_name_list,path)
