54e0ef82a9329f2524bb9ea1c783b15a130eeb2e
wl <wanglei1201@sohu.com>
2019-01-05 13:26:45 +0800

    添加启动项目脚本

--- service.sh
+++ service.sh
@@ -1,1 +1,13 @@
-123
+#!/bin/bash
+# @author: Yampery
+basepath=$(cd `dirname $0`; pwd)
+cd $basepath
+if [ !$1 ] || [ $1 == "start" ]; then
+    time=$(date "+%Y-%m-%d_%H_%M_%S") 
+    nohup python2 -u bin/run.py $2 > logs/console.log.$time 2>&1 &
+    echo "$!" > pid
+elif [ $1 == "stop" ]; then
+    kill `cat pid`
+else
+    echo "Please use [start, stop]"
+fi
