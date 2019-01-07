81103caa674da4c24afb093611f74092d4a708eb
yampery <yampery@163.com>
2019-01-07 11:17:25 +0800

    批量写入文件

--- bin/split_source_files.py
+++ bin/split_source_files.py
@@ -30,23 +30,29 @@ def split_files(files_name_list,path):
     for file_name in files_name_list:
         new_file_name_YSHJXX = "BDtaishi.YSHJXX." + datetime.datetime.now().strftime('%Y%m%d%H%M%S%f') + ".dat"
         new_file_name_TYTSMB = "BDtaishi.TYTSMB." + datetime.datetime.now().strftime('%Y%m%d%H%M%S%f') + ".dat"
+        ys_list = []    # 写入YSHJXX
+        ty_list = []    # 写入TYTSMB
         with open(path + "/" + file_name, 'r') as f:
             lines = f.readlines()
             for line in lines:
                 if "YSHJXX" in line:
-                    fo1 = open(path + "/" + new_file_name_YSHJXX, 'a')
                     str_list1 = line.split(',')[8:]
-                    str1 = ",".join(str_list1)
-                    fo1.write(str1)
-                    fo1.close()
+                    str1 = ",".join(str_list1) + '\n'
+                    ys_list.append(str1)
+
                 elif "TYTSMB" in line:
-                    fo2 = open(path + "/" + new_file_name_TYTSMB, 'a')
                     str_list2 = line.split(',')[8:]
-                    str2= ",".join(str_list2)
-                    fo2.write(str2)
-                    fo2.close()
+                    str2= ",".join(str_list2) + '\n'
+                    ty_list.append(str2)
                 else:
                     pass
+            # 循环结束，判断连个list如果不为空，则打开相应的文件将list追加进文件
+            if ys_list:
+                with open(path + "/" + new_file_name_YSHJXX, 'a') as f:
+                    f.writelines(ys_list)
+            if ty_list:
+                with open(path + "/" + new_file_name_TYTSMB, 'a') as f:
+                    f.writelines(ty_list)
 
 
 if __name__ == '__main__':
